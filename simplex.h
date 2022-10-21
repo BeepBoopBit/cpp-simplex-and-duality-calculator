8bcc84189960082087387ea6461ac76e3f8fe05c
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-10-21 18:59:35 +0800

    Created SimplexMethod class

--- simplex.h
+++ simplex.h
@@ -1,7 +1,20 @@
 #ifndef SIMPLEX_H
 #define SIMPLEX_H
 
+#include <vector>
+#include <iostream>
 
+class SimplexMethod{
+public: // Constructor
+    SimplexMethod(){
+
+    }
+
+public:
+
+private: // Variables
+    std::vector<int> _constants;
+};
 
 
 #endif
